22c04e6b93432054ce3cfdd534554b51a56f480f
tobias1012 <29657183+tobias1012@users.noreply.github.com>
2026-01-07 16:32:16 +0100

    add pipes, not working yet

--- src/main/scala/riscv/RV32I.scala
+++ src/main/scala/riscv/RV32I.scala
@@ -12,12 +12,6 @@ class RV32ITop extends Module {
 class RV32I extends Module {
   val io = IO(new Bundle {})
 
-  val IF_stage = RegInit(0.U(32.W))
-  val ID_stage = RegInit(0.U(32.W))
-  val EX_stage = RegInit(0.U(32.W))
-  val MEM_stage = RegInit(0.U(32.W))
-  val WB_stage = RegInit(0.U(32.W))
-
   val pc = RegInit(0.U(32.W))
 
   val regFile = Module(new RegisterFile())
@@ -34,17 +28,25 @@ class RV32I extends Module {
   rd2 := regFile.io.readData2.bits
 
   // IF
-
+  val instr = Wire(Valid(UInt(32.W))) // TODO: fetch instruction from memory
+  instr.valid := true.B
   // Hardcoded to addi x1, x0, 0x123
-  val instr = 0x12300093.U(32.W) // TODO: fetch instruction from memory
+  instr.bits := 0x12300093.U
+  val IF_ID = Pipe(instr)
   // ID
-  val decoder = Module(new Decoder)
-  decoder.io.instr := instr
-  // TODO: give the register file the registers to read
-  regFile.io.readReg1.bits := decoder.io.rs1
-  regFile.io.readReg2.bits := decoder.io.rs2
+  val ID_EQ = Pipe(IF_ID)
+  ID_EQ.valid := IF_ID.valid
+  when(IF_ID.valid) {
+    val decoder = Module(new Decoder)
+    decoder.io.instr := IF_ID.bits
+    // TODO: give the register file the registers to read
+    regFile.io.readReg1.bits := decoder.io.rs1
+    regFile.io.readReg2.bits := decoder.io.rs2
+  }
 
   // EX
+  val EX_MEM = Pipe(ID_EQ)
+  EX_MEM.valid := ID_EQ.valid
 
   decoder.io.imm := DontCare // TODO: connect immediate generator
   val _a1 = decoder.io.aluInput1 // TODO: use ALU input 1
@@ -52,6 +54,8 @@ class RV32I extends Module {
   val _ao = decoder.io.aluOp // TODO: use ALU operation
 
   // MEM
+  val MEM_WB = Pipe(EX_MEM)
+  MEM_WB.valid := EX_MEM.valid
 
   // WB
 
