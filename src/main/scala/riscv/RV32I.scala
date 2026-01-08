0f3d8df4b4cc0409be01edb30945ddeb4ed5968a
tobias1012 <29657183+tobias1012@users.noreply.github.com>
2026-01-08 13:32:21 +0100

    Minimal RV32I

--- src/main/scala/riscv/RV32I.scala
+++ src/main/scala/riscv/RV32I.scala
@@ -2,50 +2,46 @@ package riscv
 
 import chisel3._
 import chisel3.util._
+import java.util.Base64.Decoder
 
 class RV32ITop extends Module {
   val io = IO(new Bundle {})
 
   val cpu = Module(new RV32I())
-
 }
 
 class RV32I extends Module {
-  val io = IO(new Bundle {})
 
   val pc = RegInit(0.U(32.W))
 
   val regFile = Module(new RegisterFile())
   regFile.io.writeData := DontCare
 
-  val rd1 = WireDefault(0.U(32.W))
-  val rd2 = WireDefault(0.U(32.W))
-  rd1 := regFile.io.readData1
-  rd2 := regFile.io.readData2
-
-  // Caches for instruction and data memory
-  val ICache = Module(
-    new Cache(numLines = 64, lineSize = 64)
-  ) // 4KB instruction cache
+  val rd1 = regFile.io.readData1
+  val rd2 = regFile.io.readData2
 
   // IF
-  ICache.io.addr := pc
-  ICache.io.writeEnable := false.B
-  ICache.io.writeData := DontCare
+  val instr = 0x12300093.U(32.W) // Dummy instruction: addi x1, x0, 0x123
+  pc := pc + 4.U
+  // ID
+  val decoder = Module(new Decoder())
+  decoder.io.instr := instr
 
-  // Hardcoded to addi x1, x0, 0x123
-  // val instr := 0x12300093.U
-  val instr = ICache.io.readData
-  when(!ICache.io.hit) {
-    instr.valid := false.B
-    // TODO: fetch from memory
-    ICache.io.writeEnable := true.B
-    ICache.io.writeData := 0x12300093.U
-    instr.bits := 0x12300093.U
-    instr.valid := true.B
-  }
-  when(instr.valid) {
-    pc := pc + 4.U
-  }
+  regFile.io.readReg1 := decoder.io.rs1
+  regFile.io.readReg2 := decoder.io.rs2
+
+  // EX
+  val alu = Module(new ALU())
+  val aluInput1 =
+    Mux(decoder.io.aluInput1 === ALUInput1.Rs1, rd1.asSInt, pc.asSInt)
+  val aluInput2 =
+    Mux(decoder.io.aluInput2 === ALUInput2.Rs2, rd2.asSInt, decoder.io.imm)
+  alu.io.a := aluInput1
+  alu.io.b := aluInput2
+  alu.io.op := decoder.io.aluOp
+  val aluResult = alu.io.result.asUInt
+  // MEM
+
+  // WB
 
 }
