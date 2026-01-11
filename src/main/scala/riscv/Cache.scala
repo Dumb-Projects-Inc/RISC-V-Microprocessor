a24693dc42dba872ea501bbbbdf820d5f8ddbfc4
tobias1012 <29657183+tobias1012@users.noreply.github.com>
2026-01-11 19:05:19 +0100

    new cache setup, temporary local ROM

--- src/main/scala/riscv/Cache.scala
+++ src/main/scala/riscv/Cache.scala
@@ -59,47 +59,96 @@ class Cache(numLines: Int, lineSize: Int) extends Module {
 // 0x0000_1000 - 0x0000_FFFF : Peripherals 60kb
 // 0x0000_0000 - 0x0000_0FFF : ROM 4kb
 
+object MemoryRegions extends ChiselEnum {
+  val ROM, Peripherals, ProgramMemory = Value
+}
+
 object MemoryMap {
-  val RomStart = 0x00000000
+  // val RomStart = 0x00000000
   val RomEnd = 0x00000fff
-  val PeripheralsStart = 0x00001000
+  // val PeripheralsStart = 0x00001000
   val PeripheralsEnd = 0x0000ffff
-  val ProgramMemoryStart = 0x00010000
-  val ProgramMemoryEnd = 0x00700000
+  // val ProgramMemoryStart = 0x00010000
+  // val ProgramMemoryEnd = 0x00700000 // External memory decides how much is available
 }
 
-class CacheRequest extends Bundle {
-  val addr = UInt(32.W)
-  val writeData = UInt(1024.W)
-  val dataLength = UInt(3.W)
-  val writeEnable = Bool()
-}
-
-class CacheController extends Module {
+class CacheController(ROMProgram: String) extends Module {
   val io = IO(new Bundle {
-    // Define cache controller IO here
-    val instrAddr = Input(UInt(32.W))
-    val request = Input(new CacheRequest())
-    val stall = Output(Bool())
-    val flush = Input(Bool()) // fence.i instruction
+    val instrPort = Flipped(new instrPort())
+    val dataPort = Flipped(new dataPort())
+    // val flush = Input(Bool()) // fence.i instruction
   })
 
-  val L1ICache = Module(
-    new Cache(numLines = 4, lineSize = 1024)
-  ) // long lines, short cache, 1024 bytes = 256 instructions per line, still 4Kb
-  val L1DCache = Module(
-    new Cache(numLines = 4, lineSize = 1024)
-  ) // 4KB data cache
+  io.instrPort.instr := DontCare
+  io.dataPort.dataRead := DontCare
 
-  L1ICache.io.addr := io.instrAddr
+  val romWords = ROMProgram
+    .split("\\R")
+    .map(_.trim)
+    .filter(_.nonEmpty)
+    .map(h => BigInt(h, 16).U(32.W))
+    .toSeq
+
+  val ROM = VecInit(romWords) // Simple ROM implementation for BIOS
+
+  // Check region of instruction fetch
+  def getRegion(addr: UInt): MemoryRegions.Type =
+    MuxCase(
+      MemoryRegions.ProgramMemory,
+      Seq(
+        (addr <= MemoryMap.RomEnd.U) -> MemoryRegions.ROM,
+        (addr <= MemoryMap.PeripheralsEnd.U) -> MemoryRegions.Peripherals
+      )
+    )
+
+  val instrRegion = getRegion(io.instrPort.addr)
+  val dataRegion = getRegion(io.dataPort.addr)
+  io.instrPort.stall := false.B
+  io.dataPort.stall := false.B
+
+  val L1ICache = Module(new Cache(numLines = 256, lineSize = 4))
   L1ICache.io.writeEnable := false.B
   L1ICache.io.writeData := DontCare
+  L1ICache.io.addr := DontCare
 
-  L1DCache.io.addr := io.request.addr
-  L1DCache.io.writeEnable := io.request.writeEnable
-  L1DCache.io.writeData := io.request.writeData
+  when(io.instrPort.enable) {
+    when(instrRegion === MemoryRegions.ROM) {
+      io.instrPort.instr := ROM(io.instrPort.addr(31, 2)) // word aligned
 
-  val stall = !(L1ICache.io.hit && L1DCache.io.hit)
-  io.stall := stall
+    }.elsewhen(instrRegion === MemoryRegions.Peripherals) {
+      // instructions from peripherals not supported
+      // TODO: Exceptions
+      io.instrPort.instr := 0.U
+      io.instrPort.stall := true.B // stall indefinitely for now
+    }.otherwise {
+      // Instruction fetch from Program Memory
+      L1ICache.io.addr := io.instrPort.addr
+      L1ICache.io.writeEnable := false.B // instruction fetch is read-only
+      io.instrPort.instr := L1ICache.io.readData.bits
+      io.instrPort.stall := !L1ICache.io.readData.valid && !L1ICache.io.hit
+    }
+  }
+
+  val L1DCache = Module(new Cache(numLines = 64, lineSize = 16))
+  L1DCache.io.writeEnable := false.B
+  L1DCache.io.writeData := DontCare
+  L1DCache.io.addr := DontCare
+  when(io.dataPort.enable) {
+    when(dataRegion === MemoryRegions.ROM) {
+      // Data access to ROM (should be read-only)
+      io.dataPort.dataRead := ROM(io.dataPort.addr(31, 2))
+      io.dataPort.stall := false.B
+    }.elsewhen(dataRegion === MemoryRegions.Peripherals) {
+      // Data access to Peripherals
+    }.otherwise {
+      // Data access to Program Memory
+      L1DCache.io.addr := io.dataPort.addr
+      L1DCache.io.writeEnable := io.dataPort.writeEn
+      L1DCache.io.writeData := io.dataPort.dataWrite
+      io.dataPort.dataRead := L1DCache.io.readData.bits
+      io.dataPort.stall := !L1DCache.io.readData.valid && !L1DCache.io.hit
+
+    }
+  }
 
 }
