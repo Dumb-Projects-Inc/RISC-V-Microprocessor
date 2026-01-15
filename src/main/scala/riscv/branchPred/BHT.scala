1a65555b7324e9bfb1b7a6f6711c1f225669f32e
RKildegaard <rasmuskildegaard20@gmail.com>
2026-01-15 15:38:08 +0100

    Initial design for BHT

--- src/main/scala/riscv/branchPred/BHT.scala
+++ src/main/scala/riscv/branchPred/BHT.scala
@@ -12,4 +12,31 @@ class BHT(entries: Int) extends Module {
     val taken = Input(Bool())
     val updatePc = Input(UInt(32.W))
   })
+
+  val indexBits = log2Ceil(entries)
+  val table = RegInit(VecInit(Seq.fill(entries)(1.U(2.W))))
+
+  def getIndex(pc: UInt): UInt = pc(1 + indexBits, 2)
+
+  io.pred := table(getIndex(io.currentPc))
+
+  // Update
+  when(io.update) {
+    val idx = getIndex(io.updatePc)
+    when(io.taken) {
+      when(table(idx) =/= 3.U) {
+        table(idx) := table(idx) + 1.U
+      }.otherwise {
+        table(idx) := 3.U
+      }
+    }.elsewhen(!io.taken) {
+      when(table(idx) =/= 0.U) {
+        table(idx) := table(idx) - 1.U
+      }.otherwise {
+        table(idx) := 0.U
+      }
+    }
+    
+  }
+
 }
