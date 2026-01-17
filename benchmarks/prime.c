4888fe2ddf0860656551752683c6dcb791329cf0
tobias1012 <29657183+tobias1012@users.noreply.github.com>
2026-01-17 17:32:56 +0100

    benchmark code, should be in a working state, will test on FPGA soon

--- benchmarks/prime.c
+++ benchmarks/prime.c
@@ -1,15 +1,21 @@
-
 // USER INPUT: CSR_TIMING and UART_ADDR
-#define CSR_EN TRUE
+// #define CSR_EN TRUE
 #define UART_ADDR 0x10000000
+// #define DEBUG_EN
 
-#define NUM_PRIMES 100
+#define NUM_PRIMES 25
 
 #define BOOL int
 #define TRUE 1
 #define FALSE 0
 
-#define INT_MAX 4294967295U
+#define STARTBYTE 0xFF
+#define ENDBYTE 0xFE
+#define CSRByte 0xC5
+
+// ###############################
+//  UTILS
+// ###############################
 
 static inline int read_cycle(void)
 {
@@ -18,27 +24,100 @@ static inline int read_cycle(void)
     return value;
 }
 
-static inline void uart_write_char(char c)
+unsigned int squared(unsigned int x)
 {
-    volatile int *uart_stat = (int *)(UART_ADDR + 4);
-    volatile char *uart = (char *)UART_ADDR;
-
-    *uart = c;
+    unsigned int result = 0;
+    for (unsigned int i = 0; i < x; i++)
+    {
+        result += x;
+    }
+    return result;
 }
 
-static inline void uart_write_string(const char *str)
+unsigned int modulus(unsigned int a, unsigned int b)
+{
+    if (b == 0)
+    {
+        return FALSE; // division by zero
+    }
+    unsigned int temp = a;
+    while (temp >= b)
+    {
+        temp -= b;
+    }
+
+    return temp;
+}
+
+void itoa(unsigned int value, char *str)
+{
+    // convert integer to hex string, always 8 chars + null terminator
+    const char hex_chars[] = "0123456789ABCDEF";
+    for (int i = 7; i >= 0; i--)
+    {
+        str[i] = hex_chars[modulus(value, 16)];
+        value >>= 4;
+    }
+    str[8] = '\0';
+}
+
+// ###############################
+// UART UTILS
+// ###############################
+// CAREFULLY REMOVING PADDING, SOME CPUS MAY REQUIRE MEMORY ALIGNMENT, SO ENSURE NO VAR CROSSES MEMORY BOUNDARIES
+#pragma pack(push, 1)
+typedef struct UART
+{
+    volatile int write;
+    volatile char status;
+    char pad[3]; // padding to align to 4 bytes
+
+} UART_t;
+#pragma pack(pop)
+
+static inline void uart_write_char(UART_t *uart, char c)
+{
+    // wait until UART is ready, this is blocking, not checking if data
+    while ((uart->status & 0x1) == 0)
+        ;
+
+    uart->write = c;
+}
+
+static inline void uart_write_string(UART_t *uart, const char *str)
 {
     while (*str)
     {
-        uart_write_char(*str++);
+        uart_write_char(uart, *str++);
     }
+}
+
+void uart_write_int(UART_t *uart, unsigned int value)
+{
+    char bytes[9];
+    itoa(value, bytes);
+    uart_write_string(uart, bytes);
+}
+
+__attribute__((section(".text.start"))) __attribute__((naked)) void entry()
+{
+    // We love that no stack pointer is initialized by default
+    __asm__ volatile("li sp, 0x0040000"); // Set stack pointer to top of memory
+    __asm__ volatile("jal main");
+    __asm__ volatile("ecall"); // infinite loop after main returns
 }
 
 int main()
 {
     volatile int start_time = 0;
+    // INIT UART
+    UART_t *uart = (UART_t *)UART_ADDR;
+
 #ifdef CSR_EN
     start_time = read_cycle();
+#else
+    // send start byte to indicate benchmark start
+    uart_write_char(uart, STARTBYTE);
 #endif
 
     unsigned int primes[NUM_PRIMES];
@@ -50,36 +129,57 @@ int main()
         BOOL is_prime = TRUE;
         // calculate i^2 without multiplication
         unsigned int i = 2;
-        unsigned int i_squared = 0;
-        for (; i_squared <= num; i++)
+        unsigned int i_squared = 4;
+
+        while (i_squared <= num)
         {
-            // calculate i*i without multiplication
-            for (unsigned int j = 0; j < i; j++)
-            {
-                i_squared += i;
-            }
             // calculate num % i without division
-            unsigned int temp = num;
-            while (temp >= i)
+            unsigned int mod = modulus(num, i);
+            if (mod == 0)
             {
-                temp -= i;
-                if (temp == 0)
-                {
-                    is_prime = FALSE;
-                    break;
-                }
+                is_prime = FALSE;
+                break;
             }
-            if (is_prime)
-            {
-                primes[count++] = num;
-            }
-            num++;
+            i++;
+            i_squared = squared(i);
         }
+        if (is_prime)
+        {
+            primes[count++] = num;
+#ifdef DEBUG_EN
+            // send prime over UART
+            uart_write_string(uart, "Found prime: ");
+            uart_write_int(uart, num);
+            uart_write_string(uart, "\n");
+#endif
+        }
+        num++;
     }
 
-    volatile int end_time = 0;
 #ifdef CSR_EN
+    volatile int end_time = 0;
     end_time = read_cycle();
+    volatile int elapsed = end_time - start_time;
+    // send Data from the benchmark over UART
+    uart_write_char(uart, CSRByte); // start of benchmark data
+    uart_write_string(uart, "Start Cycles: ");
+    uart_write_int(uart, start_time);
+    uart_write_string(uart, "\nEnd Cycles: ");
+    uart_write_int(uart, end_time);
+    uart_write_string(uart, "\nElapsed Cycles: ");
+    uart_write_int(uart, elapsed);
+    uart_write_string(uart, "\nNumber of Found Primes: ");
+    uart_write_int(uart, count);
+    uart_write_string(uart, "\nPrimes:");
+    for (unsigned int i = 0; i < count; i++)
+    {
+        uart_write_string(uart, " ");
+        uart_write_int(uart, primes[i]);
+    }
+    uart_write_string(uart, "\n");
+
+#else
+    uart_write_char(uart, ENDBYTE); // end benchmark if no csr
 #endif
 
     return 0;
